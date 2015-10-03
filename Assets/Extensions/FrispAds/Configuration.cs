db4e08528b6e8e6ada931c6c4901271082a18a4d
Michael Treacher <m.w.treacher@gmail.com>
2015-10-03 10:24:08 +1000

    Add unity package for frisp ads

--- Assets/Extensions/FrispAds/Configuration.cs
+++ Assets/Extensions/FrispAds/Configuration.cs
@@ -1,25 +1,9 @@
 ﻿using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace FrispAds {
 	public class Configuration : ScriptableObject {
 		
 		public string appleAdmobId;
 		public string androidAdmobId;
-		#if UNITY_EDITOR
-		[MenuItem("Assets/Create/Configuration")]
-		public static void Create() {
-			string assetPathAndName =
-				AssetDatabase.GenerateUniqueAssetPath("Assets/Extensions/FrispAds/Configuration.asset");
-			
-			var asset = CreateInstance<Configuration>();
-			AssetDatabase.CreateAsset(asset, assetPathAndName);
-			
-			AssetDatabase.SaveAssets();
-			AssetDatabase.Refresh();
-		}
-	#endif
 	}
 }
