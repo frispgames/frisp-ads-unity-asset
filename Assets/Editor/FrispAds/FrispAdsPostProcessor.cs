696703136ebbe99ade7e3b4d3de2cb380f402ac6
Michael Treacher <m.w.treacher@gmail.com>
2015-10-25 13:58:53 +1100

    Shorten length of lines

--- Assets/Editor/FrispAds/FrispAdsPostProcessor.cs
+++ Assets/Editor/FrispAds/FrispAdsPostProcessor.cs
@@ -14,8 +14,6 @@ public class FrispAdsPostProcessor {
 		#if UNITY_IPHONE
 			if (target != BuildTarget.iOS) return;
 
-			UnityEngine.Debug.Log("PostProcessor: Copying frameworks to xcode");
-
 			string buildName = Path.GetFileNameWithoutExtension(path);
 			string pbxprojPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
 
@@ -29,16 +27,21 @@ public class FrispAdsPostProcessor {
 
 			char divider = Path.DirectorySeparatorChar;
 
-			DirectoryInfo destinationFolder = new DirectoryInfo(path + divider + "Frameworks");
+			DirectoryInfo destinationFolder =
+				new DirectoryInfo(path + divider + "Frameworks");
 		
 			foreach(DirectoryInfo file in destinationFolder.GetDirectories()) {
-				UnityEngine.Debug.Log("PostProcessor: Adding file to build - " + file.Name);
-				proj.AddFile("Frameworks/"+ file.Name, "Frameworks/"+ file.Name, PBXSourceTree.Source);
+				string filePath = "Frameworks/"+ file.Name;
+				proj.AddFile(filePath, filePath, PBXSourceTree.Source);
 				proj.AddFrameworkToProject (buildTarget, file.Name, false);
 			}
 
-			proj.SetBuildProperty(buildTarget, "FRAMEWORK_SEARCH_PATHS", "$(SRCROOT)/Frameworks");
-			proj.AddBuildProperty(buildTarget, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
+			proj.SetBuildProperty(
+				buildTarget, "FRAMEWORK_SEARCH_PATHS", "$(SRCROOT)/Frameworks"
+			);
+			proj.AddBuildProperty(
+				buildTarget, "FRAMEWORK_SEARCH_PATHS", "$(inherited)"
+			);
 			proj.SetBuildProperty(buildTarget, "CLANG_ENABLE_MODULES", "YES");
 
 			File.WriteAllText(pbxprojPath, proj.WriteToString());
